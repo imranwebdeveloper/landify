436c1fcc7ff80986bf8f05b0407dd02d193dd0c2
Imran Nazir <imran.web.developer2021@gmail.com>
2022-02-26 20:15:05 +0600

    redesign menu

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -34,7 +34,7 @@ const MobileMenuBar = ({ onConform }) => {
           </li>
         </ul>
       </div>
-      <div className=" h-16 gap-3 center">
+      <div className=" py-10 gap-3 center">
         <a href="/">
           <img src={google} alt="Apple Store" />
         </a>
